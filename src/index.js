8fdc59c464e42d87c12bfce252ebf1c6abb6f7b5
Ez <Ezrabl42@gmail.com>
2018-08-06 03:10:37 -0400

    get rid of node-sass

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+//index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
