aa2e4554a7c78d32af3002f23e4515f08bfe5cc5
Ez <Ezrabl42@gmail.com>
2018-08-07 03:29:39 -0400

    Add sass, start deciding background colors

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
-import App from './App';
+import 'styles/styles.css';
+import App from 'components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
